30f21e1efa17420f8afe68f6005a10d60a7535d4
rishitpatel <rishit.patel@outlook.com>
2017-09-17 16:51:42 -0500

    argument added

--- ssh_test.py
+++ ssh_test.py
@@ -1,4 +1,9 @@
 import ssh
+import argparse
 
+parser = argparse.ArgumentParser()
+parser.add_argument("device_name", help="Device name to collect data")
+args = parser.parse_args()
+print(args)
 connection = ssh.ssh("ip", "rishit", "pw")
-connectin.sendCommand('vbash -c -i "show system memory cache"')
+connection.sendCommand('vbash -c -i "show system memory cache"')
