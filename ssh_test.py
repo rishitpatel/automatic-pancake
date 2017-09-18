21d0816df6f18e37b23af289cc6427c169ec195b
rishit <rishit.patel@outlook.com>
2017-09-17 21:52:46 -0500

    new files

--- ssh_test.py
+++ ssh_test.py
@@ -1,9 +1,31 @@
 import ssh
 import argparse
+import yaml
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument("device_name", help="Device name to collect data")
 args = parser.parse_args()
 print(args)
-connection = ssh.ssh("ip", "rishit", "pw")
-connection.sendCommand('vbash -c -i "show system memory cache"')
+
+with open('/home/rishit/device_details/device_details.yaml') as f:
+    devices =yaml.safe_load(f)
+    f.close()
+
+device_list = devices.keys()
+
+if args not in device_list:
+    print ("Device is not in the list. Please enter valid device list.")
+    return
+else:
+    device_details = devices[args]
+
+ip = device_details['ip']
+user = device_details['username']
+passwd = device_details['password']
+devtype = device_details['type']
+
+print(ip,user,passwd,devtype)
+
+#connection = ssh.ssh("ip", "rishit", "pw")
+#connection.sendCommand('vbash -c -i "show system memory cache"')
