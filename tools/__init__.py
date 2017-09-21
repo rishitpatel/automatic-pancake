814120de11d03d6aa17fdd7f5cd5e7a5b91c3b04
rishit <rishit.patel@outlook.com>
2017-09-21 13:35:50 -0500

    new files

---
+++ tools/__init__.py
@@ -0,0 +1,3 @@
+from .dev_param import dev_param
+from .cmdline import cmdline
+from .sshlogin import Sshlogin
---
+++ tools/cmdline.py
@@ -0,0 +1,6 @@
+_author_ = 'Rishit Patel'
+
+class cmdline():
+
+    def _init_(self):
+        print("this is the cmd class")
