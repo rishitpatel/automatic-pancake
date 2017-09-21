c870a2bd2b3567f72605ff216f15cb1874da39d4
rishit <rishit.patel@outlook.com>
2017-09-21 16:41:34 -0500

    new files

--- tools/__init__.py
+++ tools/__init__.py
@@ -1,3 +1,3 @@
-from .dev_param import dev_param
-from .cmdline import cmdline
-from .sshlogin import Sshlogin
+#from .dev_param import dev_param
+#from .cmdline import cmdline
+#from .sshlogin import Sshlogin
