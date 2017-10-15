904be3bbd79b81447379425c6fa375fdfba61441
rishit <rishit.patel@outlook.com>
2017-10-14 21:16:04 -0500

    new files

--- tools/cmdline.py
+++ tools/cmdline.py
@@ -1,6 +1,9 @@
 _author_ = 'Rishit Patel'
+from tools.sshlogin import Sshlogin
 
-class cmdline():
+class cmdline(Sshlogin):
 
     def _init_(self):
         print("this is the cmd class")
+    # TODO add execute class here before adding commands
+    def execute(self,command):
