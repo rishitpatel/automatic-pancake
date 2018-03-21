6fb7531e9fe2d9fe1fbc9f8b5f2f652135e8d9e3
rishit <rishit.patel@outlook.com>
2018-03-21 11:40:51 -0500

    new file

--- Scripts/web_scrap.py
+++ Scripts/web_scrap.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup as bsoup
 from urllib import request as req
+from datetime import datetime
 url = 'https://finance.yahoo.com/quote/AAPL?p=AAPL'
-url_acc = req(url)
 url_acc = req.urlopen(url)
 url_html = url_acc.read()
 url_acc.close()
@@ -9,10 +9,21 @@ url_soup = bsoup(url_html, "html.parser")
 
 
 containers = url_soup.findAll("div", {"id": "quote-header-info"})
-len(containers)
+#print(len(containers))
 
 
 stock_name = containers[0].findAll("h1", {"data-reactid": "7"})
-stock_name[0].text
-stock_name[0].text.strip()
-stock_name[0].text.strip(''')
+stock_name = stock_name[0].text
+print (stock_name)
+stock_name_l = stock_name.split('-')
+stock_sym = stock_name_l[0].strip()
+stock_company = stock_name_l[1].strip()
+#print (stock_name[0].text.strip())
+#print (containers)
+
+print(stock_name_l)
+print(stock_sym)
+print(stock_company)
+
+stock_price = containers[0].findAll("span", {"data-reactid": "14"})
+print (stock_price[0].text)
